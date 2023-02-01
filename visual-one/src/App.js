760015ef9c80adb4eabffac4d1aa81df90b15ed3
Cole <deoncole77@gmail.com>
2023-02-01 16:25:16 -0500

    updated App.js page to remove comments

--- visual-one/src/App.js
+++ visual-one/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { ReactDOM } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -14,22 +13,6 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
